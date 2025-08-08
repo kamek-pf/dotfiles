f539e8b17e32811d20aebda2bffa3d978f9bd2dc
kamek_sie <kamek@havenstudios.com>
2025-08-08 17:28:19 -0400

    agenix fold

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -1,14 +1,18 @@
 { settings, ... }:
 let
+  # Read secrets from the source record
+  # This turns `{"something.age": ...}` into `["something"]`
   secretsFile = import ../secrets/secrets.nix;
-  secretNames = builtins.map
-    (name: builtins.elemAt (builtins.split "\\.age" name) 0)
-    (builtins.attrNames secretsFile);
+  readName = name: builtins.substring 0 (builtins.stringLength name - 4) name;
+  secretNames = builtins.map readName (builtins.attrNames secretsFile);
 
+  # Build the record expected bye agenix
+  # This generates `{"something".file: ../secrets/something.age;}` 
   makeSecrets = builtins.foldl' doMerge { };
   doMerge = acc: fileName: acc // {
     "${fileName}".file = ../secrets/${fileName}.age;
   };
+
 in
 {
   age = {
