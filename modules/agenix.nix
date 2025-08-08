8b4930d047b7d981a7fa2c67e6d7ca3d6dbd6d41
kamek_sie <kamek@havenstudios.com>
2025-08-08 17:18:14 -0400

    refactor: simplify secret name extraction with comment explanation

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -1,10 +1,11 @@
 { settings, ... }:
 let
   secretsFile = import ../secrets/secrets.nix;
-  secretNames = builtins.map 
+  # Can you split on ".age" and take the left part AI!
+  secretNames = builtins.map
     (name: builtins.substring 0 (builtins.stringLength name - 4) name)
     (builtins.attrNames secretsFile);
-    
+
   makeSecrets = builtins.foldl' doMerge { };
   doMerge = acc: fileName: acc // {
     "${fileName}".file = ../secrets/${fileName}.age;
